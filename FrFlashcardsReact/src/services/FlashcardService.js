69132f6f6e9912bd2a1defe0a31a572906eaaf87
anbruzga <60633443+anbruzga@users.noreply.github.com>
2024-06-24 15:08:36 +0100

    Tidy up FlashcardService.js

--- FrFlashcardsReact/src/services/FlashcardService.js
+++ FrFlashcardsReact/src/services/FlashcardService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-console.log('API_URL:', API_URL);  // Add this line
+
+console.log('API_URL:', API_URL);
 
 export const getAllFlashcards = () => {
     return axios.get(API_URL);
